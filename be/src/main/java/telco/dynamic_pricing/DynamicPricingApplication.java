14f9d697c90f35311e48b7ce441843f1e78eea7d
fadhliazhar14 <fadhliazhar44@gmail.com>
2026-01-14 18:49:03 +0700

    Setup properties

---
+++ be/src/main/java/telco/dynamic_pricing/DynamicPricingApplication.java
@@ -0,0 +1,13 @@
+package telco.dynamic_pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicPricingApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/telco/dynamic_pricing/DynamicPricingApplicationTests.java
@@ -0,0 +1,13 @@
+package telco.dynamic_pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicPricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
